d79224f68190ed3b76fb2123d7fcd801bce29129
MartonLak <72521563+MartonLak@users.noreply.github.com>
2020-10-16 19:23:26 +0300

    Test

---
+++ src/main/java/ro/fastrackit/classroom/DisplayAprime.java
@@ -0,0 +1,38 @@
+package ro.fastrackit.classroom;
+
+public class DisplayAprime
+
+{
+
+    public static boolean Displayaprime(int i)
+    {
+        boolean check = false;
+
+        for(int j=2;j<=i/2;j++)
+        {
+            if(i%j==0)
+            {
+                check=true;
+                break;
+            }
+        }
+        if (check==false) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public static void main(String[] args)
+    {
+       int givennumber=13;
+       for(int j=0;j<givennumber;j++)
+       {
+           if(Displayaprime(j)==true)
+           {
+               System.out.println(j);
+           }
+       }
+    }
+
+}
