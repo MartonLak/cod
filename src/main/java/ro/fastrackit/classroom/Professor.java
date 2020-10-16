f32ca1984b59f60e0b5c925ccb5414bbdcd89310
MartonLak <72521563+MartonLak@users.noreply.github.com>
2020-10-16 13:32:09 +0300

    Test

--- src/main/java/ro/fastrackit/classroom/Professor.java
+++ src/main/java/ro/fastrackit/classroom/Professor.java
@@ -4,48 +4,25 @@ import com.sun.source.tree.WhileLoopTree;
 
 import java.time.LocalDate;
 
-class Professor {
-    String lastName;
-    String firstName;
-    LocalDate DateofBirth;
-    LocalDate Dateofhiring;
-    String emailAddress;
-    Boolean PHD;
+public class  Professor implements Human
+{
 
-    public Professor(String lastName, String firstName, Boolean PHD) {
-        this.lastName = lastName;
-        this.firstName = firstName;
-        this.PHD = PHD;
+    public void walk() {
+
     }
 
-    public String getLastName() {
-        return lastName;
+    public boolean HumanGender() {
+        return false;
     }
 
-    public String getFirstName() {
-        return firstName;
+    public int gender() {
+        return 0;
     }
 
-    public Boolean getPHD() {
-        return PHD;
+    public double getExperienceinYears() {
+        return 0;
     }
 
-    public void horn() {
-        System.out.println("Baaaamp");
-    }
+
 }
 
-class PHDProfessor extends Professor {
-    public PHDProfessor(String lastName, String firstName, Boolean PHD) {
-        super(lastName, firstName, PHD);
-    }
-
-    @Override
-    public Boolean getPHD() {
-        return super.getPHD();
-    }
-
-    public static void main(String[] args) {
-
-    }
-}
--- src/main/java/ro/fastrackit/classroom/Student.java
+++ src/main/java/ro/fastrackit/classroom/Student.java
@@ -4,78 +4,22 @@ import java.time.LocalDate;
 import java.util.Date;
 
 
-public class Student {
-
-    String lastName;
-    String firstName;
-    LocalDate DateofBirth;
-    String emailAddress;
-    Float grade;
-    Boolean hasfinished;
-
-    public Student(String lastName, String firstName, LocalDate dateofBirth) {
-        this.lastName = lastName;
-        this.firstName = firstName;
-        DateofBirth = dateofBirth;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-
-    public LocalDate getDateofBirth() {
-        return DateofBirth;
-    }
-
-    public String getEmailAddress() {
-        return emailAddress;
-    }
-
-    public void setEmailAddress(String emailAddress) {
-        this.emailAddress = emailAddress;
-    }
-
-    public Float getGrade() {
-        return grade;
-    }
-
-    public void setGrade(Float grade) {
-        this.grade = grade;
-    }
-
-    public Boolean getHasfinished() {
-        return hasfinished;
-    }
-
-    public void setHasfinished(Boolean hasfinished) {
-        this.hasfinished = hasfinished;
-    }
-
-    static void myStaticMethod() {
-
-        Student stud1 = new Student("Jon", "Smith", LocalDate.now());
-        System.out.println(stud1.DateofBirth);
-        System.out.println(stud1.lastName);
-        System.out.println(stud1.firstName);
-        stud1.grade = 12f;
-        stud1.emailAddress = "m.marci56@yahoo.com";
-    }
-
-    static void myStaticMethod2() {
+public class  Student implements Human
+{
+    public void walk() {
 
     }
 
-
-    public static void main(String[] args) {
-        myStaticMethod();
-
+    public boolean HumanGender() {
+        return false;
+    }
+    public int gender() {
+        return 0;
     }
 
+    @Override
+    public double getExperienceinYears() {
+        return 0;
+    }
 
 }
