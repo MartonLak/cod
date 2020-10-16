978b7916b9694e62713a2ece2a25f74d95dcb585
MartonLak <72521563+MartonLak@users.noreply.github.com>
2020-10-16 19:00:14 +0300

    Test

---
+++ src/main/java/ro/fastrackit/classroom/CheckIfPalindrome.java
@@ -0,0 +1,25 @@
+package ro.fastrackit.classroom;
+
+public class CheckIfPalindrome
+
+{
+    public static void Palindrome(int i)
+    {
+        int number1=0, number2=0,temp;
+        temp=i;
+        while(i>0)
+        {
+            number1=i%10;
+            number2=(number2*10)+number1;
+            i=i/10;
+        }
+        if(temp==number2)
+            System.out.println("palindrome number ");
+        else
+            System.out.println("not palindrome");
+    }
+
+    public static void main(String[] args) {
+        Palindrome(1334421);
+    }
+}
