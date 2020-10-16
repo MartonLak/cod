8b0f5d09dac63258531fdea8ecd53ef416230a9e
MartonLak <72521563+MartonLak@users.noreply.github.com>
2020-10-16 15:17:31 +0300

    Test

---
+++ src/main/java/ro/fastrackit/classroom/Factorial.java
@@ -0,0 +1,29 @@
+package ro.fastrackit.classroom;
+
+public class Factorial
+{
+
+
+    public static int FactorialCalulate(int i)
+    {
+         int result=1;
+
+        for (int j=2; j<=i;j++)
+        {
+            result=result*j;
+
+        }
+        return result;
+
+    }
+    static void printresult() {
+
+        int Finaresult = FactorialCalulate(1);
+        System.out.println(Finaresult);
+    }
+    public static void main(String[] args)
+    {
+        printresult();
+    }
+
+}
