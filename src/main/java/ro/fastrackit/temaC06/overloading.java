edf250d324b37394ce035b311418660f6d209acd
MartonLak <72521563+MartonLak@users.noreply.github.com>
2020-10-30 22:00:23 +0200

    Test

---
+++ src/main/java/ro/fastrackit/temaC06/overloading.java
@@ -0,0 +1,47 @@
+package ro.fastrackit.temaC06;
+
+public class overloading
+
+{
+    public void printAnything(int a) {
+        System.out.println(a);
+    }
+    public static void printAnything(long a) {
+        System.out.println(a);
+    }
+    public static void printAnything(String a) {
+        System.out.println(a);
+    }
+    public static void printAnything(Double a) {
+        System.out.println(a);
+    }
+    public static void printAnything(Object a) {
+        System.out.println(a);
+    }
+    public static void printAnything(int[] a) {
+        System.out.println(a);
+    }
+    public static void printAnything(long[] a) {
+        System.out.println(a);
+    }
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        long b = 11;
+        String c = "asd";
+        Double d = 12.2;
+        Object e = "test";
+        int[] f = {0, 2, 3};
+        long[] g = {69,88, 88, 72};
+
+        printAnything(g[0]);
+        printAnything((double) f[1]);
+        printAnything(e);
+        printAnything(d);
+        printAnything(c);
+        printAnything(b);
+        printAnything((double) a);
+
+    }
+}
