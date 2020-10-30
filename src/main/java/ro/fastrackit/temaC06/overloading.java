a07224198e2ef1979ba2d9cc603e5f8e20f897bc
MartonLak <72521563+MartonLak@users.noreply.github.com>
2020-10-30 22:08:21 +0200

    Test

--- src/main/java/ro/fastrackit/temaC06/overloading.java
+++ src/main/java/ro/fastrackit/temaC06/overloading.java
@@ -3,7 +3,7 @@ package ro.fastrackit.temaC06;
 public class overloading
 
 {
-    public void printAnything(int a) {
+    public static void printAnything(int a) {
         System.out.println(a);
     }
     public static void printAnything(long a) {
@@ -36,12 +36,12 @@ public class overloading
         long[] g = {69,88, 88, 72};
 
         printAnything(g[0]);
-        printAnything((double) f[1]);
+        printAnything( f[1]);
         printAnything(e);
         printAnything(d);
         printAnything(c);
         printAnything(b);
-        printAnything((double) a);
+        printAnything( a);
 
     }
 }
