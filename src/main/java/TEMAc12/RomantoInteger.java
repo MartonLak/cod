1adc20b097ac17b011acc02e00f97676d53ee388
MartonLak <72521563+MartonLak@users.noreply.github.com>
2020-12-10 16:14:59 +0200

    version 1

---
+++ src/main/java/TEMAc12/RomantoInteger.java
@@ -0,0 +1,73 @@
+package TEMAc12;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class RomantoInteger
+
+{
+
+    public static void romanToInt(String s)
+    {
+        char[] ch = new char[s.length()+1];
+        int[] result =new int[s.length()+1];
+        int toInt=0;
+        for(int i=0; i<s.length();i++)
+        {
+            ch[i]=s.charAt(i);
+        }
+        for(int i=0; i<s.length();i++)
+        {
+            if(ch[i]=='I')
+            {
+                result[i]=1;
+            }
+            if(ch[i]=='V')
+            {
+                result[i]=5;
+            }
+            if(ch[i]=='X')
+            {
+                result[i]=10;
+            }
+            if(ch[i]=='L')
+            {
+                result[i]=50;
+            }
+            if(ch[i]=='C')
+            {
+                result[i]=100;
+            }
+            if(ch[i]=='D')
+            {
+                result[i]=500;
+            }
+            if(ch[i]=='M')
+            {
+                result[i]=1000;
+            }
+        }
+
+        for(int i=1; i<=s.length();i++)
+        {
+            if(result[i-1]>result[i])
+            {
+                toInt=toInt+result[i-1];
+            }
+            if (result[i-1]==result[i])
+            {
+                toInt=toInt+result[i-1];
+            }
+
+            if (result[i-1]<result[i])
+            {
+                toInt=toInt-result[i-1];
+            }
+        }
+        System.out.println("Result="+toInt);
+
+    }
+    public static void main(String[] args)
+    {
+        romanToInt("MCX");
+    }
+}
