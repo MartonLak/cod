d56d5e1435b3e3054d4c51a991b0d3d2c972daa5
MartonLak <72521563+MartonLak@users.noreply.github.com>
2020-12-18 21:06:51 +0200

    Test

---
+++ src/main/java/ro/fastrackit/classroom/Student.java
@@ -0,0 +1,41 @@
+package ro.fastrackit.classroom;
+
+public class Student
+{
+    private String firstName;
+    private String lastname;
+    private int age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
